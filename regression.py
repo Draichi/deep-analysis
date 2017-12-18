b812fdfe39df51ef546f75153587497ff3c78d63
Lucas Draichi <lucasdraichi@gmail.com>
2017-12-18 13:19:56 -0200

    updating regerssion

--- regression.py
+++ regression.py
@@ -1,8 +1,11 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pylot as plt
+from matplotlib import style
+
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
@@ -20,11 +23,14 @@ forecast_out = int(math.ceil(0.01*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 X = preprocessing.scale(X)
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -35,5 +41,8 @@ clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+# print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
 
