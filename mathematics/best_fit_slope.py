339bd4fbfa5efc61ecc7e0c0dbb7ccc74073d557
Lucas Draichi <lucasdraichi@gmail.com>
2017-12-19 16:43:25 -0200

    adding classification

--- mathematics/best_fit_slope.py
+++ mathematics/best_fit_slope.py
@@ -2,11 +2,25 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([3,4,5,6,8,9])
-ys = np.array([6,7,9,12,13,15])
+''' xs = np.array([3,4,5,6,8,9])
+ys = np.array([6,7,9,12,13,15]) '''
+
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_sloap_and_intercept(xs, ys):
     m = (  ( (mean(xs) * mean(ys)) - mean(xs*ys) ) /
@@ -14,6 +28,17 @@ def best_fit_sloap_and_intercept(xs, ys):
 
     b = mean(ys) - ( m * mean(xs) )
     return m, b
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+xs, ys = create_dataset(60, 10, 2, correlation='pos')
 
 m, b = best_fit_sloap_and_intercept(xs,ys)
 
@@ -25,7 +50,10 @@ regression_line = [ (m*x)+b for x in xs ]
 predict_x = 11
 predict_y = (m*predict_x)+b
 
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
+
 plt.scatter(xs,ys)
-plt.scatter(predict_x,predict_y)
+plt.scatter(predict_x, predict_y)
 plt.plot(xs, regression_line)
 plt.show()
---
+++ mathematics/classification.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
+
+# breast-cancer-wisconsin data from:
+# https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
+
+# reading data
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+# ? is for unknow attributes
+df.replace('?', -99999, inplace=True)
+# id is not relevant
+df.drop(['id'], 1, inplace=True)
+
+# features => everything less class
+X = np.array(df.drop(['class'], 1))
+# labels => 2 = benign, 4 = malignant
+y = np.array(df['class'])
+
+# traning data
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+# testing
+accuracy = clf.score(X_test, y_test)
+print('Accuracy: ',accuracy)
+
+# sample data
+exemple_measures = np.array([[4,2,1,1,1,2,3,2,1],[8,2,1,4,1,2,3,2,1]])
+exemple_measures = exemple_measures.reshape(len(exemple_measures),-1)
+
+prediction = clf.predict(exemple_measures)
+print('Prediction: ',prediction)
