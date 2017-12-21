18aba032b0c5325079be7fffa55532c8eda08d5d
Lucas Draichi <lucasdraichi@gmail.com>
2017-12-21 10:21:16 -0200

    finishing classification

--- mathematics/classification.py
+++ mathematics/classification.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
+from sklearn import preprocessing, cross_validation, neighbors, svm
 import pandas as pd
 
 # breast-cancer-wisconsin data from:
@@ -20,6 +20,7 @@ y = np.array(df['class'])
 # traning data
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 clf = neighbors.KNeighborsClassifier()
+# clf = svm.SVC()
 clf.fit(X_train, y_train)
 
 # testing
@@ -27,7 +28,7 @@ accuracy = clf.score(X_test, y_test)
 print('Accuracy: ',accuracy)
 
 # sample data
-exemple_measures = np.array([[4,2,1,1,1,2,3,2,1],[8,2,1,4,1,2,3,2,1]])
+exemple_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,4,1,2,3,2,1]])
 exemple_measures = exemple_measures.reshape(len(exemple_measures),-1)
 
 prediction = clf.predict(exemple_measures)
--- mathematics/euclidean_distance.py
+++ mathematics/euclidean_distance.py
@@ -16,35 +16,42 @@ def k_nearest_neighbors(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     #print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
             
-    return vote_result
+    return vote_result, confidence
 
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-full_data = df.astype(float).values.tolist()
-random.shuffle(full_data)
+accuracies = []
 
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
+for i in range(25):
+    df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+    random.shuffle(full_data)
 
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
+    test_size = 0.2
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
 
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
 
-correct = 0
-total = 0
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        total += 1
+    correct = 0
+    total = 0
 
-print('Accuracy: ',correct/total)
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+            total += 1
+
+    #print('Accuracy: ',correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))
