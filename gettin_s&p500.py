f6a44e9753d921e6f23ae6cd84712cf2a121eaf2
Draichi <lucasdraichi@gmail.com>
2018-01-16 11:47:19 -0200

    sp500 tickers

---
+++ gettin_s&p500.py
@@ -0,0 +1,19 @@
+import bs4 as bs
+import pickle
+import requests
+
+def save_sp500_list():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    # [1:] exclude the fist row (title rows)
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    print(tickers)
+    return tickers
+
+save_sp500_list()
