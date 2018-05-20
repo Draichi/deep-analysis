c487797e1bd1cc57bde09face1132e652ef3e546
Lucas Draichi <lucasdraichi@gmail.com>
2018-05-20 19:10:27 -0300

    bitcoin forecast

--- cryptos/bitcoin_forecast.py
+++ cryptos/bitcoin_forecast.py
@@ -28,9 +28,6 @@ except (OSError, IOError) as e:
         pickle.dump(df, ff)
     print('-- cached data')
 
-# print(df.head())
-# quit()
-
 """
 percent of change => ( Close - Open ) / Open * 100
 high low percent  => ( High - Close ) / Close * 100
@@ -41,16 +38,11 @@ df['PCT_Change'] = (df['Close'] - df['Open']) / df['Open'] * 100
 
 df = df[['Close', 'HL_PCT', 'PCT_Change', 'Volume (Currency)']]
 
-# print(df.tail())
-
 df.fillna(-99999, inplace = True)
 
 forecast_col = 'Close'
 
 forecast_out = int(math.ceil(0.001*len(df)))
-
-# print(df.tail())
-
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
@@ -58,8 +50,6 @@ X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
 X_lately = X[-forecast_out:]
 
-# print(df['label'])
-# quit()
 X = X[:-forecast_out]
 
 df.dropna(inplace=True)
@@ -73,11 +63,11 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 
 clf = LinearRegression(n_jobs=10)
 clf.fit(X_train, y_train)
-# with open('datasets/bitstamp.pickle', 'wb') as f:
-#     pickle.dump(clf, f)
+with open('datasets/bitstamp.pkl', 'wb') as f:
+    pickle.dump(clf, f)
 
-pickle_in = open('datasets/bitstamp.pkl', 'rb')
-clf = pickle.load(pickle_in)
+# pickle_in = open('datasets/bitstamp.pkl', 'rb')
+# clf = pickle.load(pickle_in)
 
 accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
@@ -96,22 +86,6 @@ for i in forecast_set:
     next_date = dt.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
-
-# df_ohlc = df['Last'].resample('10D').ohlc()
-# df_volume = df['Volume'].resample('10D').sum()
-# df_ohlc.reset_index(inplace=True)
-# df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-
-# ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-# ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-# ax1.xaxis_date()
-
-# candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
-
-# ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
-# df['Forecast'].plot()
-
-# plt.show()
 
 df['Close'].plot()
 df['Forecast'].plot()
