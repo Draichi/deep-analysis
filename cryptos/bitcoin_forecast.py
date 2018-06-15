014b5b7fa061d7a1714d1afe09dca054130d7c90
draichi <lucasdraichi@gmail.com>
2018-06-15 05:11:29 -0300

    wip

--- cryptos/bitcoin_forecast.py
+++ cryptos/bitcoin_forecast.py
@@ -8,7 +8,8 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-from sklearn import preprocessing, cross_validation, svm
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from matplotlib import style
 from mpl_finance import candlestick_ohlc
@@ -32,34 +33,50 @@ except (OSError, IOError) as e:
 percent of change => ( Close - Open ) / Open * 100
 high low percent  => ( High - Close ) / Close * 100
 """
+df['Last day close'] = df['Close'].shift(1)
 
 df['HL_PCT'] = (df['High'] - df['Close']) / df['Close'] * 100
 df['PCT_Change'] = (df['Close'] - df['Open']) / df['Open'] * 100
 
-df = df[['Close', 'HL_PCT', 'PCT_Change', 'Volume (Currency)']]
+# df = df[['Close', 'HL_PCT', 'PCT_Change', 'Volume (Currency)']]
 
 df.fillna(-99999, inplace = True)
 
-forecast_col = 'Close'
+# print(df.tail())
 
-forecast_out = int(math.ceil(0.001*len(df)))
+last_date = df.iloc[-1].name
+lastdate = dt.datetime.fromtimestamp(1528858800.0)
 
-df['label'] = df[forecast_col].shift(-forecast_out)
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
 
-X = np.array(df.drop(['label'], 1))
+print('timestamp', lastdate)
+print('last_date', last_date)
+print('last_unix',last_unix)
+print('next_unix',next_unix)
+# quit()
+# for i in range(3):
+#     next_date = dt.datetime.fromtimestamp(next_unix)
+#     print('next date', next_date)
+#     next_unix += one_day
+#     df.loc[next_date] = [np.nan for _ in range(len(df.columns))]
+#     print(df.loc[next_date])
+
+x_df = df.drop(['Close'], 1)
+# print(x_df.tail())
+print(x_df.loc[dt.datetime.date(last_date)])
+print(x_df.tail())
+quit()
+
+X = np.array(df.drop(['Close'], 1))
 X = preprocessing.scale(X)
-X_lately = X[-forecast_out:]
-
-X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 
-print(df.head())
-print(df.tail())
+y = np.array(df['Close'])
 
-y = np.array(df['label'])
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 clf = LinearRegression(n_jobs=10)
 clf.fit(X_train, y_train)
@@ -69,8 +86,13 @@ with open('datasets/bitstamp.pkl', 'wb') as f:
 # pickle_in = open('datasets/bitstamp.pkl', 'rb')
 # clf = pickle.load(pickle_in)
 
+next_days
+
+
 accuracy = clf.score(X_test, y_test)
-forecast_set = clf.predict(X_lately)
+forecast_set = clf.predict(next_days)
+
+quit()
 
 print('---  Accuracy:', accuracy)
 print('---  Forecast out:', forecast_out, 'days')
@@ -86,6 +108,7 @@ for i in forecast_set:
     next_date = dt.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    print(df.loc[next_date])
 
 df['Close'].plot()
 df['Forecast'].plot()
