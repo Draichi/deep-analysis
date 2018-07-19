43b864573e75b77f3b35b588980f0bd3e49678f2
draichi <lucasdraichi@gmail.com>
2018-07-19 00:26:04 -0300

    plotting

--- cryptos/coingekko.py
+++ cryptos/coingekko.py
@@ -1,34 +1,25 @@
 import pandas as pd
 import datetime
+import plotly.offline as offline
+import plotly.graph_objs as go
 
 df = pd.read_csv('ali.csv')
 # df['index'] = df['prices']
-for i, item in enumerate(df.prices):
+for i, item in enumerate(df['prices']):
     current_item = item.replace('[', '').replace(']', '').split(',')
     date = current_item[0]
     price = current_item[1]
-    df.loc[i, 'index'] = date
+    dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
+    df.loc[i, 'date'] = dt
     df.loc[i, 'price'] = price
 
-df_prices = df[['index', 'price']]
-df_prices.set_index('index')
-print(df_prices)
-quit()
+df_prices = df[['date', 'price']]
+# df_prices.reset_index(drop=True)
+df_prices.set_index('date')
 
-df.columns = ['index', 'market_cap', 'prices', 'total_volumes']
-df.set_index('index')
-item = df['total_volumes'][0]
-current_item = item.replace('[', '').replace(']', '').split(',')
-date = current_item[0]
-price = current_item[1]
+data = [go.Scatter(
+        x=df_prices['date'],
+        y=df_prices['price']
+    )]  
 
-dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
-
-label_arr = list(df)
-
-series_arr = list(map(lambda col: df[col], label_arr))
-print(df)
-quit()
-
-print('date:', dt)
-print('price:', price)
+offline.plot(data)
