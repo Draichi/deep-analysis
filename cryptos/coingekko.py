649226c24c1df161095090893c6eb0277470a2a5
draichi <lucasdraichi@gmail.com>
2018-07-20 02:26:47 -0300

    wip

--- cryptos/coingekko.py
+++ cryptos/coingekko.py
@@ -8,7 +8,7 @@ import pandas as pd
 # https://plot.ly/python/time-series/
 
 coins = ['giant', 'rupaya']
-keys = ['prices', 'market_caps', 'total_volumes']
+keys = ['prices']
 
 def get_coin_data(coin):
     try:
@@ -29,13 +29,7 @@ coin_data = {}
 for coin in coins:
     data = get_coin_data(coin)
     data.name = coin
-    print(data.name)
-    # print(data)
-    # quit()
-    # coin_data[coin] = data
-    # print(data)
     for key in keys:
-        print(key)
         for i, item in enumerate(data[key]):
             current_item = item.replace('[', '').replace(']', '').split(',')
             date = current_item[0]
@@ -57,28 +51,6 @@ for coin in coins:
     )
     data.append(trace)
 
-# print(data)
-# quit()
-
-# trace_price = go.Scatter(
-#     x=df['fantasy-gold']['date'],
-#     y=df['fantasy-gold']['prices'],
-#     name = "fantasy-gold prices",
-# )
-
-# trace_cap = go.Scatter(
-#     x=coin_data['fantasy-gold']['date'],
-#     y=coin_data['fantasy-gold']['market_caps'],
-#     name = "fantasy-gold caps",
-# )
-
-# trace_vol = go.Scatter(
-#     x=coin_data['rupaya']['date'],
-#     y=coin_data['rupaya']['prices'],
-#     name = "rupaya prices",
-# )
-
-# data = [trace_cap, trace_price]
 layout = go.Layout(
     plot_bgcolor='#010008',
     paper_bgcolor='#010008',
@@ -89,36 +61,3 @@ layout = go.Layout(
 
 offline.plot({'data': data, 'layout': layout})
 
-
-
-
-
-
-
-
-
-
-
-# for i, item in enumerate(df['prices']):
-#     current_item = item.replace('[', '').replace(']', '').split(',')
-#     date = current_item[0]
-#     price = current_item[1]
-#     dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
-#     df.loc[i, 'date'] = dt
-#     df.loc[i, 'price'] = price
-
-# for i, item in enumerate(df['market_caps']):
-#     current_item = item.replace('[', '').replace(']', '').split(',')
-#     date = current_item[0]
-#     cap = current_item[1]
-#     dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
-#     df.loc[i, 'date'] = dt
-#     df.loc[i, 'cap'] = cap
-
-# for i, item in enumerate(df['total_volumes']):
-#     current_item = item.replace('[', '').replace(']', '').split(',')
-#     date = current_item[0]
-#     vol = current_item[1]
-#     dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
-#     df.loc[i, 'date'] = dt
-#     df.loc[i, 'vol'] = vol
